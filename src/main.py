5f019ad86e8bee48c1bb1ec7d00224d8af5cdf52
Mario Coloma <marioxcolomar@gmail.com>
2026-05-02 14:53:37 +0200

    Add function to main for copying static sources to public directory.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
-from textnode import TextNode
+import shutil
+from pathlib import Path
+
 
 def main():
-    text = TextNode("This is some anchor text", "link", "https://boot.dev")
-    print(text)
+    source_dir = Path("static")
+    destination_dir = Path("public")
+    copy_directory(source_dir, destination_dir)
+
+
+def copy_directory(source, destination):
+    if not source.exists() or not source.is_dir():
+        raise ValueError(f"source directory does not exists: {source}")
+    # empty the destination directory before beginning
+    if destination.exists():
+        shutil.rmtree(destination)
+
+    destination.mkdir(parents=True, exist_ok=True)
+
+    def copy(curr_source, curr_destination):
+        for item in curr_source.iterdir():
+            target = curr_destination / item.name
+
+            if item.is_dir():
+                target.mkdir(parents=True, exist_ok=True)
+                copy(item, target)
+            else:
+                shutil.copy2(item, target)
+                print(f"copied: {item} -> {target}")
+
+    copy(source, destination)
+
 
 if __name__ == "__main__":
     main()

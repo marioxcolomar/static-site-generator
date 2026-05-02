da560a36a5669a9e10d387b393d8e01186397d22
Mario Coloma <marioxcolomar@gmail.com>
2026-05-02 04:04:24 +0200

    Fix matcher in text node. Mode text node to html node method outside of text node class.

--- src/textnode.py
+++ src/textnode.py
@@ -19,30 +19,30 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        match = self.text_type == other.text_type
+        match = (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
         if match:
             return True
         return False
 
-    def text_node_to_html_node(self, text_node):
-        match TextType(text_node):
-            case TextType.TEXT:
-                return LeafNode(None, text_node.text)
-
-            case TextType.BOLD:
-                return LeafNode("b", text_node.text)
-
-            case TextType.ITALIC:
-                return LeafNode("i", text_node.text)
-
-            case TextType.CODE:
-                return LeafNode("code", text_node.text)
-
-            case TextType.LINK:
-                return LeafNode("a", text_node.text)
-
-            case _:
-                raise Exception(f"Error: text node unknown {text_node}")
-
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise Exception(f"Error: text node unknown {text_node.text_type}")
